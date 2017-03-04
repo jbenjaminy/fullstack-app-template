c1c96374dcb9b046ad2de0e1d2c393c52c76c7ac
jbenjaminy <young.benjamin@gmail.com>
2017-03-04 15:51:09 -0600

    modularizes redux

--- client-side/redux/reducers/reducer.js
+++ client-side/redux/reducers/reducer.js
@@ -1,26 +1,48 @@
-//import * as actions from './actions';
+import _ from 'lodash';
+import {
+	FORM_UPDATE,
+	LOGIN_USER_START,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL,
+	FETCH_PROFILE_SUCCESS,
+	FETCH_ANECDOTES_SUCCESS,
+	CLEAR_ANECDOTES,
+	REPORT_FAILURE
+} from '../actions/types';
 
-const initial_state = {
-	spam: 'ham',
-	paula: 'brillant [sic]',
+const INITIAL_STATE = {
+	auth: { email: '', password: '', loading: false, error: '', user: null },
+	profile: { name: '', age: '', sex: '', country: '' },
+	anecdotes: [{q: 'What should I do?', a: 'Add some anecdotes!', uid: 'anecdote-default'}]
 };
 
-export default function reducer(state=initial_state, action={}) {
+export default (state = INITIAL_STATE, action = {}) => {
 	switch (action.type) {
-		case 'SPAMIFY_SPAM':
-			return {...state, spam: 'spam'};
-		case 'HAMIFY_SPAM':
-			return {...state, spam: 'ham'};
-		case 'REPLACE_PAULA':
-			return {...state, paula: action.paula};
-		case 'FETCH_HELLO_SUCCESS':
-			console.log("The server says:", action.message);
-			break;
-		case 'REPORT_FAILURE':
-			console.error("Failure in " + action.what + ":");
-			console.error(action.error);
-			break;
-		default: break;
+		case FORM_UPDATE:
+            return { ...state, [action.data.prop]: action.data.value };
+        case LOGIN_USER_START: {
+			// const { loading, error } = state.auth;
+            // return { ...state, loading: true, error: '' };
+		}
+		case LOGIN_USER_SUCCESS:
+			return { ...state, ...INITIAL_STATE.auth, user: action.data };
+		case LOGIN_USER_FAIL:
+            return { ...state, error: 'Authentication failure.', password: '', loading: false };
+		case FETCH_PROFILE_SUCCESS:
+			return { ...state, profile };
+		case FETCH_ANECDOTES_SUCCESS: {
+			const anecdotes = _.map(action.data, (val, uid) => {
+				return { ...val, uid };
+			});
+			return { ...state, anecdotes };
+		}
+		case CLEAR_ANECDOTES:
+			return { ...state, ...INITIAL_STATE.anecdotes };
+		case REPORT_FAILURE:
+            console.error(`${action.type} failure:`);
+            console.error(action.error);
+            break;
+        default: break;
 	}
 	return state;
 }
