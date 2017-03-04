d48338ac97e0a50ddafb3d7ae35deb17f1509302
jbenjaminy <young.benjamin@gmail.com>
2017-03-03 23:55:10 -0600

    adds redux directory and moves associated files

--- client/components/main-component.jsx
+++ client/components/main-component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import NewPaula from './new-paula';
 
 class MainComponent extends React.Component {
--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainComponent from './main-component';
-import store from './store';
 import {Provider} from 'react-redux';
+import store from './redux/store';
+import MainComponent from './components/main-component';
 
 document.addEventListener('DOMContentLoaded', () =>
 	ReactDOM.render(<Provider store={store}>
