e66d8f4eec3d50e1ba4181cd289dc91e27e4d5dc
jbenjaminy <young.benjamin@gmail.com>
2017-03-03 23:56:57 -0600

    updates index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/store';
 import MainComponent from './components/main-component';
 
-document.addEventListener('DOMContentLoaded', () =>
-	ReactDOM.render(<Provider store={store}>
-		<MainComponent />
-	</Provider>, document.getElementById('app'))
-);
+document.addEventListener('DOMContentLoaded', () => {
+		ReactDOM.render(
+			<Provider store={store}>
+				<MainComponent />
+			</Provider>, document.getElementById('app')
+	);
+});
