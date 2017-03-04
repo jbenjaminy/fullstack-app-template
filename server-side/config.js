8fa0fa9ada926a44610b35a6f80c2c1b898629ee
jbenjaminy <young.benjamin@gmail.com>
2017-03-04 01:09:54 -0600

    adds config.js file for firebase

---
+++ server-side/config.js
@@ -0,0 +1,17 @@
+/**
+ *  Getting Started with Firebase:
+    *   Sign into firebase with a Google account (https://firebase.google.com/).
+    *   Click 'Go to console'.
+    *   Click 'Create new project'.
+    *   Name your project.
+    *   Click 'Add Firebase to your web app'.
+    *   Populate the below fields with your project-specific information.
+ */
+ 
+export default {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    storageBucket: '',
+    messagingSenderId: ''
+};
