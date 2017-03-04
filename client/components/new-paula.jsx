7eb5f274503c62e38e633ea2d5116308eb044e30
jbenjaminy <young.benjamin@gmail.com>
2017-03-03 23:59:16 -0600

    adds actions to connect function in component

--- client/components/new-paula.jsx
+++ client/components/new-paula.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from './actions';
 
-export default connect()(class NewPaula extends React.Component {
+export default connect(actions)(class NewPaula extends React.Component {
 	update(e) {
 		e.preventDefault();
-		this.props.dispatch(actions.replace_paula(this.refs.paula.value));
-		this.props.dispatch(actions.fetch_hello());
+		this.props.replace_paula(this.refs.paula.value);
+		this.props.actions.fetch_hello();
 	}
 
 	render() {
