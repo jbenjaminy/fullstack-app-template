e29eec35b576682a4f3942216d1b865d4157f5f1
jbenjaminy <young.benjamin@gmail.com>
2017-03-04 00:59:48 -0600

    adds server-side directory, renames client to client-side, updates server.js

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-//NOTE: ES6 'import' statements cannot be used in the server - just use ES5
-//syntax instead ('require'). Other ES6 features should all be available.
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const jsonParser = bodyParser.json();
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-app.use(express.static("build"));
-
-app.get("/hello", (req, res) => {
-	res.json({message: "Hello, world!"});
+app.use(express.static('build'));
+app.use(jsonParser);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
 });
 
-function run_server() {
+app.post('/register', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	res.json({ message: 'Hello, world!' });
+});
+
+app.get('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	res.json({ message: 'Hello, world!' });
+});
+
+function runServer() {
 	app.listen(PORT, HOST, err => {
 		if (err) return console.error(err);
 		console.log(`Listening on port ${PORT}`);
 	});
 }
 
-if (require.main === module) run_server();
+if (require.main === module) runServer();
